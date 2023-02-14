e5b13b1f94ee742701d16dde72cf3491cc2d8c2f
Algo <algor102@gmail.comm>
2023-02-15 04:59:41 +0500

    Двумерный массив с элементами A=m+n

---
+++ dz2-dvumArrAmn/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void PrintArray(int[,] arr)
+{
+  for (int i = 0; i<arr.GetLength(0); i++)
+  {
+    for (int j = 0; j<arr.GetLength(1); j++)
+    {
+      Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int IntRead(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int m = IntRead("Введите количество строк в массиве");
+int n = IntRead("Введите количество столбцов в массиве");
+int[,] array = new int[m,n];
+for (int i = 0; i<array.GetLength(0); i++)
+{
+  for (int j = 0; j<array.GetLength(1); j++)
+  {
+      array[i,j] = i + j;
+  }
+}
+
+PrintArray(array);
